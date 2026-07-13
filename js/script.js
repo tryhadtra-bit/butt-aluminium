2275cb3a40fa8be029ebaa8f10534e05d285e8c7
tryhadtra-bit <tryhadtra@gmail.com>
2026-07-13 18:28:21 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -59,9 +59,9 @@ reveal.classList.add("active");
 
 });
 const header = document.querySelector("header");
-
 let lastScroll = 0;
 
+window.addEventListener("scroll", function () {
 
     if (!header) return;
 
@@ -75,9 +75,10 @@ let lastScroll = 0;
             header.style.top = "0";
         }
 
+    } else {
+        header.style.top = "0";
     }
 
     lastScroll = currentScroll;
 
-
-    if (!header) return;
+});
